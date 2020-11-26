d90d29c26595b4e2912879af2a2158f4eb304245
Sparsh Agarwal <sparshaga25@gmail.com>
2020-11-26 14:45:30 +0530

    Added a Person image for missing cast images

--- src/components/CastRow.jsx
+++ src/components/CastRow.jsx
@@ -1,24 +1,28 @@
 import React, { useEffect, useState } from "react";
 import axios from "./../utils/axios";
 import "./../styles/CastRow.css";
+import person from "./../assets/person_icon.png";
 
 const baseUrl = "https://image.tmdb.org/t/p/original";
 
 function CastRow({ id }) {
-  const url = `/movie/${id}/credits`;
+  //   const url = `/movie/${id}/credits`;
 
   const [cast, setCast] = useState([]);
 
   useEffect(() => {
     async function getCastDetails() {
-      const request = await axios.get(url, {
-        params: {
-          api_key: process.env.REACT_APP_API_KEY,
-          language: "en-US",
-        },
-      });
+      const request = await axios.get(
+        `/movie/${id}/credits`,
+        {
+          params: {
+            api_key: process.env.REACT_APP_API_KEY,
+            language: "en-US",
+          },
+        }
+      );
 
-      console.log(request.data.cast);
+      //   console.log(request.data.cast);
       setCast(request.data.cast);
       return request;
     }
@@ -37,9 +41,17 @@ function CastRow({ id }) {
               key={castMember.cast_id}
             >
               <img
-                src={`${baseUrl}${castMember.profile_path}`}
+                src={
+                  castMember.profile_path
+                    ? `${baseUrl}${castMember.profile_path}`
+                    : person
+                }
                 alt={castMember.name}
-                className="cast-member-img"
+                className={
+                  castMember.profile_path
+                    ? "cast-member-img"
+                    : "person-img"
+                }
               />
               <h5 className="cast-member-name">
                 {castMember.name}
